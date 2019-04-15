d04703e66e33382dba25b10d40dd377d2fad85f5
shengbo wang <shuichen17@gmail.com>
2019-04-15 12:54:35 -0700

    finish

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
-class ForagerBee {
+class ForagerBee extends Bee {
   // TODO..
+  constructor () {
+  	super ();
+  	this.age = 10;
+  	this.job = 'find pollen';
+  	this.canFly =true;
+  	this.treasureChest = [];
+
+  }
+  forage () {
+  	var treasure = 1;
+  	this.treasureChest.push(treasure);
+  	return this.treasureChest;
+  }
 };
